fe9729917a77b10b7fb74aed6d62843f0d7fc500
Hojun Lee <050218jun@gmail.com>
2024-12-27 15:20:28 -0800

    Add icons, sound, and gauge

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let interval = null;
+let gaugeInterval = null;
 let eggState = -1; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const frySound = document.getElementById('frySound');
     const plateSound = document.getElementById('plateSound');
-
+    
     // start frying
     document.getElementById('smallEgg').addEventListener('click', () => {
         const egg = document.getElementById('egg');
+        let color = 'red';
+        let gaugePercent = 0;
         
         frySound.play();
 
         egg.style.display = 'block';
         eggState = 0;
         updateEggAppearance(egg, eggState);
-
+        
         if (!interval) {
-    
             interval = setInterval(() => {
-    
+            
             eggState++;
             updateEggAppearance(egg, eggState);
     
@@ -29,6 +31,31 @@ document.addEventListener('DOMContentLoaded', () => {
     
             }, 10000); // every 10s
         }
+
+        if (!gaugeInterval) {
+            gaugeInterval = setInterval(() => {
+                gaugePercent += 0.25;
+
+                if (eggState === 1) color = 'green';
+                if (eggState === 2) color = 'yellow';
+                if (eggState === 3) color = 'orange';
+                if (eggState === 4) color = 'red';
+
+                console.log('GaugePercent:', gaugePercent, 'Color:', color);
+                
+                const column = document.querySelector('.column');
+
+                column.style.background = color;
+                column.style.height = gaugePercent + '%';
+
+                if (gaugePercent >= 100) {
+                    clearInterval(gaugeInterval);
+                    gaugeInterval = null;
+                }
+
+            }, 100)
+        }
+        
     });
     
     // put egg on plate
@@ -36,8 +63,11 @@ document.addEventListener('DOMContentLoaded', () => {
         const kitchen = document.getElementById('kitchen');
         const pView = document.getElementById('pView');
         
+        plateSound.pause();
+        plateSound.currentTime = 0; // Reset to the start
+        plateSound.play();
+
         if (eggState === -1) {
-            plateSound.play();
 
             pView.style.display = 'flex';
             kitchen.style.display = 'none';
@@ -49,7 +79,6 @@ document.addEventListener('DOMContentLoaded', () => {
         else {
             frySound.pause();
             frySound.currentTime = 0;
-            plateSound.play();
 
             pView.style.display = 'flex';
             kitchen.style.display = 'none';
@@ -60,6 +89,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const eggImg = document.createElement('div');
             eggImg.style.backgroundImage = `url(assets/egg_${eggState}.png)`;
             eggImg.className = 'egg';
+            eggImg.style.width = '96px';
+            eggImg.style.height= '96px';
 
             plate.appendChild(eggImg);
         }
@@ -70,7 +101,14 @@ document.addEventListener('DOMContentLoaded', () => {
         plateSound.pause();
         clearInterval(interval);
         interval = null;
+        clearInterval(gaugeInterval);
+        gaugeInterval = null;
+        gaugePercent = 0;
         eggState = -1;
+
+        const column = document.querySelector('.column');
+        column.style.height = '0%'; // Reset height
+        column.style.background = 'white';
 
         const kitchen = document.getElementById('kitchen');
         const pView = document.getElementById('pView');
